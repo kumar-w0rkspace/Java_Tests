be14a25d57db7c3a075fc429da1edeb7615c9994
kumar <worksp4ce_beginings@proton.me>
2025-04-08 11:45:26 +0530

    day 5 session

--- day005_b_Jump_Control_Statements/Xeg_AB_Continue.java
+++ day005_b_Jump_Control_Statements/Xeg_AB_Continue.java
@@ -3,7 +3,7 @@ package day005_b_Jump_Control_Statements;
 public class Xeg_AB_Continue {
 
 	public static void main(String[] args) {
-		// Print one to ten and skip 3, 5, 9
+		// Print one to ten while skip 3, 5, 9
 		
 		for (int i = 1; i <= 10; i ++) {
 			
